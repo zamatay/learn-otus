9b96369767cb2ea66e6ff552c6c8a9f80c0756a8
ZamuraevA <zamuraevA@yandex.ru>
2023-06-18 14:01:24 +0300

    add unicode sring to test

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -20,6 +20,7 @@ func TestUnpack(t *testing.T) {
 		{input: `qwe\45`, expected: `qwe44444`},
 		{input: `qwe\\5`, expected: `qwe\\\\\`},
 		{input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe♬3`, expected: `qwe♬♬♬`},
 	}
 
 	for _, tc := range tests {
