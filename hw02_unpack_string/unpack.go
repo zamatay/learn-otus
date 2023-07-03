f702fc689e9ab029698c823a423598f201b2aab7
Александр Замураев <a.zamuraev@automacon.ru>
2023-07-03 18:53:07 +0300

    del main

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,16 +2,9 @@ package hw02unpackstring
 
 import (
 	"errors"
-	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
-
-type Data struct {
-	prevRune      rune
-	prev2Rune     rune
-	stringBuilder strings.Builder
-}
 
 func Unpack(_ string) (string, error) {
 	return "", nil
