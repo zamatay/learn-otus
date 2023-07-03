ff4994644feb5136427616d4d3a4353afb6600df
Александр Замураев <a.zamuraev@automacon.ru>
2023-07-03 19:00:03 +0300

    check

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,10 +2,107 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode/utf8"
 )
+
+type Data struct {
+	prevRune      rune
+	prev2Rune     rune
+	stringBuilder strings.Builder
+}
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	return "", nil
+func Unpack(s string) (string, error) {
+	if s == "" {
+		return "", nil
+	}
+	var r Data
+	for index, currentRune := range s {
+		if index == 0 && isDigit(currentRune) {
+			return "", ErrInvalidString
+		}
+		if index == utf8.RuneCountInString(s)-1 && isSlash(currentRune) && !isSlash(r.prevRune) {
+			return "", ErrInvalidString
+		}
+		if !r.getNextItem(currentRune) {
+			return "", ErrInvalidString
+		}
+	}
+	return r.stringBuilder.String(), nil
+}
+
+func isDigit(r rune) bool {
+	return r >= 48 && r <= 57
+}
+
+func isSlash(r rune) bool {
+	return r == '\\'
+}
+
+func isPrint(r rune) bool {
+	return !isSlash(r) && !isDigit(r) && r != 0
+}
+
+func (r *Data) checkItemIsNotValid(currentRune rune) bool {
+	return isDigit(r.prev2Rune) && isDigit(r.prevRune) || !isSlash(r.prev2Rune) && isDigit(currentRune) && isDigit(r.prevRune)
+}
+func (r *Data) getNextItem(currentRune int32) bool {
+	if r.checkItemIsNotValid(currentRune) {
+		return false
+	}
+	if r.prevRune > 0 && r.prev2Rune > 0 {
+		r.printValue(currentRune)
+	} else {
+		r.setPrevRune(currentRune)
+	}
+	return true
+}
+
+func (r *Data) printValue(currentRune rune) {
+	item1, item2, item3 := r.getItems(currentRune)
+	switch {
+	case isPrint(item1) && !isDigit(item2):
+		r.addItem(item1, currentRune, 1, 1)
+	case isPrint(item1) && isDigit(item2):
+		r.addItem(item1, currentRune, item2, 2)
+	case isSlash(item1) && !isPrint(item2) && isDigit(item3):
+		r.addItem(item2, currentRune, item3, 3)
+	case isSlash(item1) && !isPrint(item2):
+		r.addItem(item2, currentRune, 1, 2)
+	}
+}
+
+func (r *Data) setPrevRune(currentRune rune) {
+	r.prev2Rune = r.prevRune
+	r.prevRune = currentRune
+}
+
+func (r *Data) getItems(currentRune rune) (rune, rune, rune) {
+	switch {
+	case r.prev2Rune != 0:
+		return r.prev2Rune, r.prevRune, currentRune
+	case r.prevRune != 0:
+		return r.prevRune, currentRune, 0
+	default:
+		return currentRune, 0, 0
+	}
+}
+
+func (r *Data) addItem(item, currentRune rune, count interface{}, countShift int) {
+	if item != 0 {
+		cnt, _ := count.(int)
+		t := strings.Repeat(string(item), cnt)
+		r.stringBuilder.WriteString(t)
+	}
+
+	switch countShift {
+	case 1:
+		r.prev2Rune, r.prevRune = r.prevRune, currentRune
+	case 2:
+		r.prev2Rune, r.prevRune = 0, currentRune
+	case 3:
+		r.prev2Rune, r.prevRune = 0, 0
+	}
 }
