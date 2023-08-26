5c03ba56e3e06c3a5599aa6585fde92029fc3bd4
Александр Замураев <a.zamuraev@automacon.ru>
2023-08-26 22:54:14 +0300

    06

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,28 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := in
+	for _, stage := range stages {
+		twin := make(Bi)
+
+		go func(bi Bi, out Out) {
+			defer close(twin)
+
+			for {
+				select {
+				case value, isOpen := <-out:
+					if !isOpen {
+						return
+					}
+					bi <- value
+				case <-done:
+					return
+				}
+			}
+		}(twin, out)
+
+		out = stage(twin)
+	}
+
+	return out
 }
