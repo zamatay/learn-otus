29a0b94ba21c2be3c4caa4bf2046585736489e75
aleksandr <zamuraevav@mail.ru>
2023-09-09 16:15:14 +0300

    task 07

--- hw07_file_copying/main.go
+++ hw07_file_copying/main.go
@@ -17,7 +17,6 @@ func init() {
 	flag.StringVar(&to, "to", "", "file to write to")
 	flag.Int64Var(&limit, "limit", 0, "limit of bytes to copy")
 	flag.Int64Var(&offset, "offset", 0, "offset in input file")
-
 }
 
 func FileExists(fileName string) bool {
