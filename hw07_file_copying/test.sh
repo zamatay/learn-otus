08695563e1b510b5033925c551275e40593b30d6
aleksandr <zamuraevav@mail.ru>
2023-09-12 21:38:59 +0300

    task 07

--- hw07_file_copying/test.sh
+++ hw07_file_copying/test.sh
@@ -21,5 +21,11 @@ cmp out.txt testdata/out_offset100_limit1000.txt
 ./go-cp -from testdata/input.txt -to out.txt -offset 6000 -limit 1000
 cmp out.txt testdata/out_offset6000_limit1000.txt
 
+./go-cp -from testdata/input.txt -to out.txt -offset 6000
+cmp out.txt testdata/out_offset6000.txt
+
+./go-cp -from testdata/input.txt -to out.txt -offset 6000 -limit 100
+cmp out.txt testdata/out_offset6000_limit100.txt
+
 rm -f go-cp out.txt
 echo "PASS"
