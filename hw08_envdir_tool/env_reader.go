eb1248d370d2ae92a196cd697dd0fed7d9dc2c43
aleksandr <zamuraevav@mail.ru>
2023-09-12 21:04:22 +0300

    task 07

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"bufio"
+	"errors"
+	"log"
+	"os"
+	"path"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -8,9 +17,55 @@ type EnvValue struct {
 	NeedRemove bool
 }
 
+var ErrDirIsNotLoad = errors.New("DirectoryIsNotLoad")
+
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		log.Println("cant load dir")
+		return nil, ErrDirIsNotLoad
+	}
+	result := make(Environment, len(entries))
+	for _, file := range entries {
+		if file.IsDir() || strings.Contains(file.Name(), "=") {
+			continue
+		}
+		fileName := path.Join(dir, file.Name())
+		file, err := os.Open(fileName)
+		if err != nil {
+			log.Println("Ошибка при чтении файла\n %v", err)
+		}
+		scanner := bufio.NewScanner(file)
+		ev := EnvValue{}
+		ev.NeedRemove = scanner.Scan()
+		if ev.NeedRemove {
+			result[file.Name()] = ev
+			continue
+		}
+		ev.Value = scanner.Text()
+		file.Close()
+		ev.Value = strings.TrimRight(ev.Value, " \t")
+		ev.Value = strings.ReplaceAll(ev.Value, "\x00", "\n")
+		ev.NeedRemove = len(ev.Value) == 0
+		result[file.Name()] = ev
+	}
+	return result, nil
+}
+
+func SetEnv(envs Environment) {
+	for key, env := range envs {
+		if env.NeedRemove {
+			err := os.Unsetenv(key)
+			if err != nil {
+				log.Fatalf("Не смог удалить %s", key)
+			}
+			continue
+		}
+		err := os.Setenv(key, env.Value)
+		if err != nil {
+			log.Fatalf("Не смог установить %s", key)
+		}
+	}
 }
