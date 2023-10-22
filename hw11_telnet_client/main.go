3fea0cc06714990225b3d4dac65c055b3dad46a1
Александр Замураев <a.zamuraev@automacon.ru>
2023-10-22 20:07:08 +0300

    check me

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,35 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"log/slog"
+	"os"
+	"time"
+)
+
+type Options struct {
+	timeout time.Duration
+	host    string
+	port    string
+}
+
+var programOptions Options
+
+func init() {
+	flag.DurationVar(&programOptions.timeout, "timeout", 100, "timeout duration")
+}
+
 func main() {
-	// Place your code here,
+	if !flag.Parsed() {
+		flag.Parse()
+	}
+	slog.Info("Flag", slog.Duration("timeout", programOptions.timeout))
+	if len(os.Args) < 3 {
+		log.Fatal("hostAndPortInvalid")
+	}
+	programOptions.host = os.Args[len(os.Args)-2]
+	programOptions.port = os.Args[len(os.Args)-1]
+
 	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"log"
+	"net"
 	"time"
 )
 
@@ -12,10 +15,46 @@ type TelnetClient interface {
 	Receive() error
 }
 
+var conn net.Conn
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
 	// Place your code here.
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+type tc struct {
+}
+
+func (t tc) Connect() error {
+	connectionStr := fmt.Sprintf("%s:%s", programOptions.host, programOptions.port)
+	var err error
+	conn, err = net.Dial("tcp", connectionStr)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+	return nil
+}
+
+func (t tc) Close() error {
+	conn.Close()
+}
+
+func (t tc) Send() error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (t tc) Receive() error {
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	response := string(buf[:n])
+	fmt.Println(response)
+
+	return nil
+}
