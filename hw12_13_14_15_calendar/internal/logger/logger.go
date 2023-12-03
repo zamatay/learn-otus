dc5cf5fd943a7b4a77f6ba4c2fac06487ad7da4c
Александр Замураев <a.zamuraev@automacon.ru>
2023-12-03 09:55:05 +0300

    hw10

--- hw12_13_14_15_calendar/internal/logger/logger.go
+++ hw12_13_14_15_calendar/internal/logger/logger.go
@@ -1,20 +1,52 @@
 package logger
 
-import "fmt"
+import (
+	"log"
+	"log/slog"
+	"os"
+)
 
-type Logger struct { // TODO
+type Logger struct {
+	*slog.Logger // TODO
 }
+
+// Log
+var Log Logger
 
 func New(level string) *Logger {
-	return &Logger{}
+	var l slog.Level
+	switch level {
+	case "debug":
+		l = slog.LevelDebug
+	case "info":
+		l = slog.LevelInfo
+	case "warn":
+		l = slog.LevelWarn
+	default:
+		l = slog.LevelError
+	}
+	return &Logger{
+		slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: l})),
+	}
 }
 
-func (l Logger) Info(msg string) {
-	fmt.Println(msg)
+func (l Logger) Debug(msg string, args ...any) {
+	l.Debug(msg, args)
 }
 
-func (l Logger) Error(msg string) {
-	// TODO
+func (l Logger) Info(msg string, args ...any) {
+	l.Info(msg, args)
 }
 
-// TODO
+func (l Logger) Error(msg string, args ...any) {
+	l.Error(msg, args)
+}
+
+func (l Logger) Warn(msg string, args ...any) {
+	l.Warn(msg, args)
+}
+
+func (l Logger) Fatal(msg string) {
+	log.Fatal(msg)
+}
